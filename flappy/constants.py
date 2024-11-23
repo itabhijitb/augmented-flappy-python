3511255bae4c963da86ae18619ccdf0a83539224
abhijitbhattacharjee <abhijitbhattacharjee@salesforce.com>
2024-11-23 15:54:54 +0530

    Add Animation to Sprite

---
+++ flappy/constants.py
@@ -0,0 +1,49 @@
+# Font settings
+FONT_FAMILY = "JurassicPark"
+FONT_SIZE_LARGE = 30
+FONT_SIZE_MEDIUM = 20
+
+# Color scheme
+BACKGROUND_COLOR = "black"
+BUTTON_COLOR = "#63F5FF"
+TEXT_COLOR = "white"
+
+# File paths
+EXCEL_FILE_PATH = "res/leaderboard.xlsx"
+LOGO_IMAGE_PATH = "images/logo.png"
+SCHOOL_IMAGE_PATH = "images/NHPS.png"
+FONT_FILE_PATH = "res/JurassicPark.otf"
+SPRITE_PIPES = "images/pipe.png"
+SPRITE_BIRD = "images/pterodactyl.png"
+SPRITE_LOGO = "images/logo.png"
+
+# Default messages
+LEADERBOARD_LOAD_ERROR = ["Error loading leaderboard data."]
+LEADERBOARD_FILE_NOT_FOUND = ["Leaderboard file not found."]
+ERROR_NAME_REQUIRED = "Please enter your name."
+ERROR_CLASS_SECTION_REQUIRED = "Please enter both Class and Section."
+
+# Dropdown Options
+ROLE_OPTIONS = ["Teacher", "Student"]
+
+
+# Game Configuration
+PIPE_DISTANCE_BETWEEN = 500
+PIPE_SPACE_BETWEEN = 500
+PIPE_SPAWN_INTERVAL = 40
+STAGE_INTERVAL = 10  # Time in seconds before stage increases
+
+
+# Bird Configuration
+BIRD_WIDTH = 50
+BIRD_HEIGHT = 37
+
+
+# Face Tracking Configuration
+MIN_DETECTION_CONFIDENCE = 0.5
+MIN_TRACKING_CONFIDENCE = 0.5
+
+# Colors
+ # Black
+
+
